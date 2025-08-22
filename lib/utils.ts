bad0a207666364899605193896a7fd3ec1d05726
Neelam Sadat <neelamsayed100@gmail.com>
2025-08-22 18:26:49 +0430

    vapi solve

--- lib/utils.ts
+++ lib/utils.ts
@@ -54,8 +54,8 @@ export const configureAssistant = (voice: string, style: string) => {
                 },
             ],
         },
-        clientMessages: [],
-        serverMessages: [],
+        clientMessages: [] as any,
+        serverMessages: [] as any,
     };
     return vapiAssistant;
 };
